44f3d3dc0c9bb630a3e133b789bf6738d5afa748
giorgias18 <giorgiaseghetto18@gmail.com>
2026-04-27 18:29:35 +0200

    Rinomina submodule in suonacongigi-frontend

--- suonacongigi-backend/src/main/java/it/generation/suonacongigi/SuonaConGigiApplication2.java
+++ suonacongigi-backend/src/main/java/it/generation/suonacongigi/SuonaConGigiApplication2.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SuonaConGigiApplication {
+public class SuonaConGigiApplication2 {
 
     public static void main(String[] args) {
-        SpringApplication.run(SuonaConGigiApplication.class, args);
+        SpringApplication.run(SuonaConGigiApplication2.class, args);
     }
 }
